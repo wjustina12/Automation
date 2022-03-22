26f1cbb0924139236ef5d1ffafe3db8d359ec0b1
Justina Williams <wjustina12@gmail.com>
2022-03-22 17:14:40 +0000

    Made modifications to configuration_backup

--- Nornir-Scrapli-Automation/Cisco/Operational_Commands/Show_Commands/Cisco_Runbooks/configuration_backup.py
+++ Nornir-Scrapli-Automation/Cisco/Operational_Commands/Show_Commands/Cisco_Runbooks/configuration_backup.py
@@ -1,19 +1,21 @@
-from bcrypt import absolute_import
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 import os.path
+from os import makedirs
 from datetime import date
 from rich import print as rprint 
 
-nr = InitNornir(config_file="/home/jwilliams/Nornir-Scrapli-Automation/Cisco/Cisco_Inventory/config_file.yaml")
+nr = InitNornir(config_file="/home/jwilliams/Automation/Nornir-Scrapli-Automation/Cisco/Cisco_Inventory/config_file.yaml")
 
 def write_to_file(task):
    start_config = task.run(task=send_command, command="show start")
    
-   path = "/home/jwilliams/Nornir-Scrapli-Automation/Cisco/Configuration_Backup/"
+   path = f"/home/jwilliams/Automation/Nornir-Scrapli-Automation/Cisco/Configuration_Backup/{date.today()}"
    file_name = f"{task.host}_configuration"
    absolute_name = os.path.join(path, file_name)
+   path_check = os.makedirs(path, exist_ok=True)
+
    with open(absolute_name, mode='w') as config_file:
        config_file.write(start_config)
 
--- Nornir-Scrapli-Automation/Cisco/Operational_Commands/Show_Commands/Cisco_Runbooks/show_configuration.py
+++ Nornir-Scrapli-Automation/Cisco/Operational_Commands/Show_Commands/Cisco_Runbooks/show_configuration.py
@@ -1,3 +1,17 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
+from rich import print as rprint
+
+nr = InitNornir(config_file="/home/jwilliams/Automation/Nornir-Scrapli-Automation/Cisco/Cisco_Inventory/config_file.yaml")
+
+def write_configuration(task):
+    start_config = task.run(task=send_command, command="show start")
+    task.host['facts'] = start_config.scrapli_response.textfsm_parse_output()
+    configuration = task.host['facts']
+    rprint(configuration)
+    
+
+show_configuration = nr.run(task=write_configuration)
+
+
